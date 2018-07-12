66219b7bf6e6c1ce8a5e531f0f41da4ed6454f64
Diego Leal <diegoleallinux@gmail.com>
2018-07-12 16:20:29 -0400

    newtons cradle simulation

---
+++ newtonsCradle/script.js
@@ -0,0 +1,48 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Body = Matter.Body,
+    Composites = Matter.Composites,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    World = Matter.World;
+
+    // create engine
+    var engine = Engine.create(),
+        world = engine.world;
+
+    // create renderer
+    var render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600,
+            showVelocity: true
+        }
+    });
+
+    Render.run(render);
+
+    // create runner
+    var runner = Runner.create();
+    Runner.run(runner, engine);
+
+    var cradle = Composites.newtonsCradle(280, 100, 5, 30, 200);
+    World.add(world, cradle);
+    Body.translate(cradle.bodies[0], { x: -180, y: -100});
+
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+          mouse: mouse,
+          constraint: {
+            stiffness: 0.2,
+            render: {
+              visible: false
+            }
+          }
+        });
+
+    World.add(world, mouseConstraint);
+
+    render.mouse = mouse;
